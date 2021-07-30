punitagrawal/rteval

commit fd3b732f714da07d5b5326489b6e4aec1bc4fb00

rteval: 2to3 transformations
Applied a series of fixes from the 2to3 tool, as well as a few of my own
tweaks

Signed-off-by: John Kacur <jkacur@redhat.com>

rteval/rtevalConfig.py
@@ -31,9 +31,9 @@
 #   are deemed to be part of the source code.
 #
 import os, sys
-import ConfigParser
-from Log import Log
-from systopology import SysTopology
+import configparser
+from .Log import Log
+from .systopology import SysTopology
 
 def get_user_name():
     name = os.getenv('SUDO_USER')
@@ -111,7 +111,7 @@ class rtevalCfgSection(object):
         "Simple method for dumping config when object is used as a string"
         if len(self.__cfgdata) == 0:
             return "# empty"
-        return "\n".join(["%s: %s" % (k,v) for k,v in self.__cfgdata.items()]) + "\n"
+        return "\n".join(["%s: %s" % (k,v) for k,v in list(self.__cfgdata.items())]) + "\n"
 
 
     def __setattr__(self, key, val):
@@ -119,22 +119,22 @@ class rtevalCfgSection(object):
 
 
     def __getattr__(self, key):
-        if key in self.__cfgdata.keys():
+        if key in list(self.__cfgdata.keys()):
             return self.__cfgdata[key]
         return None
 
 
     def items(self):
-        return self.__cfgdata.items()
+        return list(self.__cfgdata.items())
 
 
     def __iter__(self):
         "Initialises the iterator loop"
-        self.__dict__['_rtevalCfgSection__iter_list'] = self.__cfgdata.keys()
+        self.__dict__['_rtevalCfgSection__iter_list'] = list(self.__cfgdata.keys())
         return self
 
 
-    def next(self):
+    def __next__(self):
         "Function used by the iterator"
 
         if not self.__dict__['_rtevalCfgSection__iter_list'] \
@@ -153,16 +153,16 @@ class rtevalCfgSection(object):
 
     def has_key(self, key):
         "has_key() wrapper for the configuration data"
-        return self.__cfgdata.has_key(key)
+        return key in self.__cfgdata
 
 
     def keys(self):
         "keys() wrapper for configuration data"
-        return self.__cfgdata.keys()
+        return list(self.__cfgdata.keys())
 
 
     def setdefault(self, key, defvalue):
-        if not self.__cfgdata.has_key(key):
+        if key not in self.__cfgdata:
             self.__cfgdata[key] = defvalue
         return self.__cfgdata[key]
 
@@ -171,7 +171,7 @@ class rtevalCfgSection(object):
         if type(newdict) is not dict:
             raise TypeError('update() method expects a dict as argument')
 
-        for key, val in newdict.iteritems():
+        for key, val in newdict.items():
             self.__cfgdata[key] = val
 
 
@@ -190,10 +190,10 @@ class rtevalConfig(object):
 
         # get our system topology info
         self.__systopology = SysTopology()
-        print("got system topology: %s" % self.__systopology)
+        print(("got system topology: %s" % self.__systopology))
 
         # Import the default config first
-        for sect, vals in default_config.items():
+        for sect, vals in list(default_config.items()):
             self.__update_section(sect, vals)
 
         # Set the runtime provided init variables
@@ -201,7 +201,7 @@ class rtevalConfig(object):
             if type(initvars) is not dict:
                 raise TypeError('initvars argument is not a dict variable')
 
-            for sect, vals in initvars.items():
+            for sect, vals in list(initvars.items()):
                 self.__update_section(sect, vals)
 
 
@@ -209,7 +209,7 @@ class rtevalConfig(object):
         if not section or not newvars:
             return
 
-        if not self.__config_data.has_key(section):
+        if section not in self.__config_data:
             self.__config_data[section] = rtevalCfgSection(newvars)
         else:
             self.__config_data[section].update(newvars)
@@ -218,7 +218,7 @@ class rtevalConfig(object):
     def __str__(self):
         "Simple method for dumping config when object is used as a string"
         ret = ""
-        for sect in self.__config_data.keys():
+        for sect in list(self.__config_data.keys()):
             ret += "[%s]\n%s\n" % (sect, str(self.__config_data[sect]))
         return ret
 
@@ -236,7 +236,7 @@ class rtevalConfig(object):
             if os.path.exists(p):
                 self.__info("found config file %s" % p)
                 return p
-        raise RuntimeError, "Unable to find configfile"
+        raise RuntimeError("Unable to find configfile")
 
 
     def Load(self, fname = None, append = False):
@@ -253,13 +253,13 @@ class rtevalConfig(object):
             return
 
         self.__info("reading config file %s" % cfgfile)
-        ini = ConfigParser.ConfigParser()
+        ini = configparser.ConfigParser()
         ini.optionxform = str
         ini.read(cfgfile)
 
         # wipe any previously read config info
         if not append:
-            for s in self.__config_data.keys():
+            for s in list(self.__config_data.keys()):
                 self.__config_data[s].wipe()
 
         # copy the section data into the __config_data dictionary
@@ -308,14 +308,14 @@ class rtevalConfig(object):
 
 
     def HasSection(self, section):
-        return self.__config_data.has_key(section)
+        return section in self.__config_data
 
 
     def GetSection(self, section):
         try:
             # Return a new object with config settings of a given section
             return self.__config_data[section]
-        except KeyError, err:
+        except KeyError as err:
             raise KeyError("The section '%s' does not exist in the config file" % section)
 
 
@@ -325,10 +325,10 @@ def unit_test(rootdir):
         l.SetLogVerbosity(Log.INFO)
         cfg = rtevalConfig(logger=l)
         cfg.Load(os.path.join(rootdir, 'rteval.conf'))
-        print cfg
+        print(cfg)
         return 0
-    except Exception, e:
-        print "** EXCEPTION %s", str(e)
+    except Exception as e:
+        print("** EXCEPTION %s", str(e))
         return 1
 
 

rteval-cmd
@@ -52,7 +52,7 @@ def summarize(repfile, xslt):
         try:
             t = tarfile.open(repfile)
         except:
-            print "Don't know how to summarize %s (tarfile open failed)" % repfile
+            print("Don't know how to summarize %s (tarfile open failed)" % repfile)
             return
         element = None
         for f in t.getnames():
@@ -60,7 +60,7 @@ def summarize(repfile, xslt):
                 element = f
                 break
         if element == None:
-            print "No summary.xml found in tar archive %s" % repfile
+            print("No summary.xml found in tar archive %s" % repfile)
             return
         tmp = tempfile.gettempdir()
         t.extract(element, path=tmp)
@@ -83,7 +83,7 @@ def summarize(repfile, xslt):
 
     # Parse and print the report through the XSLT template - preserve proper encoding
     resdoc = xsltprs(xmldoc)
-    print unicode(resdoc).encode('UTF-8')
+    print(str(resdoc).encode('UTF-8'))
 
     # Clean up
     del resdoc
@@ -162,7 +162,7 @@ def parse_options(cfg, parser, cmdargs):
 
     (cmd_opts, cmd_args) = parser.parse_args(args = cmdargs)
     if cmd_opts.rteval___version:
-        print("rteval version %s" % RTEVAL_VERSION)
+        print(("rteval version %s" % RTEVAL_VERSION))
         sys.exit(0);
 
     if cmd_opts.rteval___duration:
@@ -266,7 +266,7 @@ if __name__ == '__main__':
         # if --summarize was specified then just parse the XML, print it and exit
         if rtevcfg.summarize or rtevcfg.rawhistogram:
             if len(cmd_args) < 1:
-                raise RuntimeError, "Must specify at least one XML file with --summarize!"
+                raise RuntimeError("Must specify at least one XML file with --summarize!")
 
             for x in cmd_args:
                 if rtevcfg.summarize:
@@ -277,7 +277,7 @@ if __name__ == '__main__':
             sys.exit(0)
 
         if os.getuid() != 0:
-            print "Must be root to run rteval!"
+            print("Must be root to run rteval!")
             sys.exit(-1)
 
         logger.log(Log.DEBUG, '''rteval options:
@@ -295,7 +295,7 @@ if __name__ == '__main__':
                 rtevcfg.duration, rtevcfg.sysreport))
 
         if not os.path.isdir(rtevcfg.workdir):
-            raise RuntimeError, "work directory %s does not exist" % rtevcfg.workdir
+            raise RuntimeError("work directory %s does not exist" % rtevcfg.workdir)
 
 
         rteval = RtEval(config, loadmods, measuremods, logger)

rteval/Log.py
@@ -80,26 +80,26 @@ def unit_test(rootdir):
     def run_log_test(l):
         for lt in range(min(logtypes), max(logtypes)*2):
             test = ", ".join([logtypes_s[logtypes.index(i)] for i in [p for p in takewhile(lambda x: x <= lt, (2**i for i in count())) if p & lt]])
-            print "Testing verbosity flags set to: (%i) %s" % (lt, test)
+            print("Testing verbosity flags set to: (%i) %s" % (lt, test))
             msg = "Log entry when verbosity is set to %i [%s]" % (lt, test)
             l.SetLogVerbosity(lt)
             test_log(l, msg)
-            print "-"*20
+            print("-"*20)
 
     try:
-        print "** Testing stdout"
+        print("** Testing stdout")
         l = Log()
         run_log_test(l)
 
-        print "** Testing file logging - using test.log"
+        print("** Testing file logging - using test.log")
         l = Log("test.log")
         run_log_test(l)
 
         return 0
-    except Exception, e:
+    except Exception as e:
         import traceback
         traceback.print_exc(file=sys.stdout)
-        print "** EXCEPTION %s", str(e)
+        print("** EXCEPTION %s", str(e))
         return 1
 
 

rteval/__init__.py
@@ -34,13 +34,13 @@ __license__ = "GPLv2 License"
 import os, signal, sys, threading, time
 from datetime import datetime
 from distutils import sysconfig
-from modules.loads import LoadModules
-from modules.measurement import MeasurementModules, MeasurementProfile
-from rtevalReport import rtevalReport
-from rtevalXMLRPC import rtevalXMLRPC
-from Log import Log
-import rtevalConfig, rtevalMailer
-import version
+from .modules.loads import LoadModules
+from .modules.measurement import MeasurementModules, MeasurementProfile
+from .rtevalReport import rtevalReport
+from .rtevalXMLRPC import rtevalXMLRPC
+from .Log import Log
+from . import rtevalConfig, rtevalMailer
+from . import version
 
 RTEVAL_VERSION = version.RTEVAL_VERSION
 
@@ -52,7 +52,7 @@ def sig_handler(signum, frame):
     if signum == signal.SIGINT or signum == signal.SIGTERM:
         global stopsig_received
         stopsig_received = True
-        print "*** stop signal received - stopping rteval run ***"
+        print("*** stop signal received - stopping rteval run ***")
     else:
         raise RuntimeError("SIGNAL received! (%d)" % signum)
 
@@ -81,7 +81,7 @@ class RtEval(rtevalReport):
         self.__reportdir = None
 
         # Import SystemInfo here, to avoid DMI warnings if RtEval() is not used
-        from sysinfo import SystemInfo
+        from .sysinfo import SystemInfo
         self._sysinfo = SystemInfo(self.__rtevcfg, logger=self.__logger)
 
         # prepare a mailer, if that's configured
@@ -105,11 +105,11 @@ class RtEval(rtevalReport):
             self.__xmlrpc = rtevalXMLRPC(self.__rtevcfg.xmlrpc, self.__logger, self.__mailer)
             if not self.__xmlrpc.Ping():
                 if not self.__rtevcfg.xmlrpc_noabort:
-                    print "ERROR: Could not reach XML-RPC server '%s'.  Aborting." % \
-                        self.__rtevcfg.xmlrpc
+                    print("ERROR: Could not reach XML-RPC server '%s'.  Aborting." % \
+                        self.__rtevcfg.xmlrpc)
                     sys.exit(2)
                 else:
-                    print "WARNING: Could not ping the XML-RPC server.  Will continue anyway."
+                    print("WARNING: Could not ping the XML-RPC server.  Will continue anyway.")
         else:
             self.__xmlrpc = None
 
@@ -122,7 +122,7 @@ class RtEval(rtevalReport):
         if hours: r = r - (hours * 3600)
         minutes = r / 60
         if minutes: r = r - (minutes * 60)
-        print "rteval time remaining: %d days, %d hours, %d minutes, %d seconds" % (days, hours, minutes, r)
+        print("rteval time remaining: %d days, %d hours, %d minutes, %d seconds" % (days, hours, minutes, r))
 
 
     def Prepare(self, onlyload = False):
@@ -135,7 +135,7 @@ class RtEval(rtevalReport):
             # or the loads logging is enabled
             if not onlyload or self.__rtevcfg.logging:
                 self.__reportdir = self._make_report_dir(self.__rtevcfg.workdir, "summary.xml")
-        except Exception, e:
+        except Exception as e:
             raise RuntimeError("Cannot create report directory (NFS with rootsquash on?) [%s]", str(e))
 
         self.__logger.log(Log.INFO, "Preparing load modules")
@@ -173,23 +173,23 @@ class RtEval(rtevalReport):
             if with_loads:
                 self._loadmods.Start()
 
-            print "rteval run on %s started at %s" % (os.uname()[2], time.asctime())
+            print("rteval run on %s started at %s" % (os.uname()[2], time.asctime()))
             onlinecpus = self._sysinfo.cpu_getCores(True)
             cpulist = self._loadmods._cfg.GetSection("loads").cpulist
             if cpulist:
-                print "started %d loads on cores %s" % (self._loadmods.ModulesLoaded(), cpulist),
+                print("started %d loads on cores %s" % (self._loadmods.ModulesLoaded(), cpulist), end=' ')
             else:
-                print "started %d loads on %d cores" % (self._loadmods.ModulesLoaded(), onlinecpus),
+                print("started %d loads on %d cores" % (self._loadmods.ModulesLoaded(), onlinecpus), end=' ')
             if self._sysinfo.mem_get_numa_nodes() > 1:
-                print " with %d numa nodes" % self._sysinfo.mem_get_numa_nodes()
+                print(" with %d numa nodes" % self._sysinfo.mem_get_numa_nodes())
             else:
-                print ""
+                print("")
             cpulist = self._measuremods._MeasurementModules__cfg.GetSection("measurement").cpulist
             if cpulist:
-                print "started measurement threads on cores %s" % cpulist
+                print("started measurement threads on cores %s" % cpulist)
             else:
-                print "started measurement threads on %d cores" % onlinecpus
-            print "Run duration: %s seconds" % str(self.__rtevcfg.duration)
+                print("started measurement threads on %d cores" % onlinecpus)
+            print("Run duration: %s seconds" % str(self.__rtevcfg.duration))
 
             # start the cyclictest thread
             measure_profile.Start()
@@ -220,7 +220,7 @@ class RtEval(rtevalReport):
 
                 if with_loads:
                     if len(threading.enumerate()) < nthreads:
-                        raise RuntimeError, "load thread died!"
+                        raise RuntimeError("load thread died!")
 
                 if not load_avg_checked:
                     self._loadmods.SaveLoadAvg()
@@ -232,14 +232,14 @@ class RtEval(rtevalReport):
                     left_to_run = stoptime - currtime
                     self.__show_remaining_time(left_to_run)
                     rpttime = currtime + report_interval
-                    print "load average: %.2f" % self._loadmods.GetLoadAvg()
+                    print("load average: %.2f" % self._loadmods.GetLoadAvg())
                 currtime = time.time()
 
             self.__logger.log(Log.DEBUG, "out of measurement loop")
             signal.signal(signal.SIGINT, signal.SIG_DFL)
             signal.signal(signal.SIGTERM, signal.SIG_DFL)
 
-        except RuntimeError, e:
+        except RuntimeError as e:
             raise RuntimeError("appeared during measurement: %s" % e)
 
         finally:
@@ -250,7 +250,7 @@ class RtEval(rtevalReport):
             if with_loads:
                 self._loadmods.Stop()
 
-        print "stopping run at %s" % time.asctime()
+        print("stopping run at %s" % time.asctime())
 
         # wait for measurement modules to finish calculating stats
         measure_profile.WaitForCompletion()

rteval/misc.py
@@ -29,7 +29,7 @@ def expand_cpulist(cpulist):
         if '-' in part:
             a, b = part.split('-')
             a, b = int(a), int(b)
-            result.extend(range(a, b + 1))
+            result.extend(list(range(a, b + 1)))
         else:
             a = int(part)
             result.append(a)
@@ -61,9 +61,9 @@ def cpuinfo():
 if __name__ == "__main__":
 
     info = cpuinfo()
-    idx = info.keys()
+    idx = list(info.keys())
     idx.sort()
     for i in idx:
-        print "%s: %s" % (i, info[i])
+        print("%s: %s" % (i, info[i]))
 
-    print "0: %s" % (info['0']['model name'])
+    print("0: %s" % (info['0']['model name']))

rteval/modules/__init__.py
@@ -211,7 +211,7 @@ class rtevalModulePrototype(threading.Thread):
         "Return libxml2.xmlNode object with the gathered timestamps"
 
         ts_n = libxml2.newNode("timestamps")
-        for k in self.__timestamps.keys():
+        for k in list(self.__timestamps.keys()):
             ts_n.newChild(None, k, str(self.__timestamps[k]))
 
         return ts_n
@@ -277,7 +277,7 @@ the information provided by the module"""
                             metavar='LIST')
         parser.add_option_group(grparser)
 
-        for (modname, mod) in self.__modsloaded.items():
+        for (modname, mod) in list(self.__modsloaded.items()):
             opts = mod.ModuleParameters()
             if len(opts) == 0:
                 continue
@@ -290,9 +290,9 @@ the information provided by the module"""
                 cfg = None
 
             grparser = optparse.OptionGroup(parser, "Options for the %s module" % shortmod)
-            for (o, s) in opts.items():
-                descr   = s.has_key('descr') and s['descr'] or ""
-                metavar = s.has_key('metavar') and s['metavar'] or None
+            for (o, s) in list(opts.items()):
+                descr   = 'descr' in s and s['descr'] or ""
+                metavar = 'metavar' in s and s['metavar'] or None
 
                 try:
                     default = cfg and getattr(cfg, o) or None
@@ -301,7 +301,7 @@ the information provided by the module"""
                     default = None
 
                 if default is None:
-                    default = s.has_key('default') and s['default'] or None
+                    default = 'default' in s and s['default'] or None
 
 
                 grparser.add_option('--%s-%s' % (shortmod, o),
@@ -353,7 +353,7 @@ returned when a ModuleContainer object is iterated over"""
 
     def GetModulesList(self):
         "Returns a list of module names"
-        return self.__modobjects.keys()
+        return list(self.__modobjects.keys())
 
 
     def GetNamedModuleObject(self, modname):
@@ -364,11 +364,11 @@ returned when a ModuleContainer object is iterated over"""
     def __iter__(self):
         "Initiates the iterating process"
 
-        self.__iter_list = self.__modobjects.keys()
+        self.__iter_list = list(self.__modobjects.keys())
         return self
 
 
-    def next(self):
+    def __next__(self):
         """Internal Python iterating method, returns the next
 module name and object to be processed"""
 
@@ -511,7 +511,7 @@ start their workloads yet"""
                 self._logger.log(Log.DEBUG, "\t - Stopping %s" % modname)
                 if mod.is_alive():
                     mod.join(2.0)
-            except RuntimeError, e:
+            except RuntimeError as e:
                 self._logger.log(Log.ERR, "\t\tFailed stopping %s: %s" % (modname, str(e)))
         self.__timestamps['stop'] = datetime.now()
 

rteval/modules/loads/hackbench.py
@@ -128,7 +128,7 @@ class Hackbench(CommandLineLoad):
                              stderr=self.__err)
         if not p:
             self._log(Log.DEBUG, "hackbench failed to start on node %s" % node)
-            raise RuntimeError, "hackbench failed to start on node %s" % node
+            raise RuntimeError("hackbench failed to start on node %s" % node)
         return p
 
     def _WorkloadTask(self):
@@ -147,7 +147,7 @@ class Hackbench(CommandLineLoad):
                 if self.tasks[n].poll() is not None:
                     self.tasks[n].wait()
                     self.tasks[n] = self.__starton(n)
-            except OSError, e:
+            except OSError as e:
                 if e.errno != errno.ENOMEM:
                     raise e
                 # Exit gracefully without a traceback for out-of-memory errors
@@ -166,7 +166,7 @@ class Hackbench(CommandLineLoad):
             return
 
         for node in self.nodes:
-            if self.tasks.has_key(node) and self.tasks[node].poll() is None:
+            if node in self.tasks and self.tasks[node].poll() is None:
                 self._log(Log.INFO, "cleaning up hackbench on node %s" % node)
                 self.tasks[node].send_signal(SIGKILL)
                 if self.tasks[node].poll() == None:

rteval/modules/loads/kcompile.py
@@ -110,7 +110,7 @@ class Kcompile(CommandLineLoad):
 
     def _WorkloadSetup(self):
         # find our source tarball
-        if self._cfg.has_key('tarball'):
+        if 'tarball' in self._cfg:
             tarfile = os.path.join(self.srcdir, self._cfg.tarfile)
             if not os.path.exists(tarfile):
                 raise rtevalRuntimeError(self, " tarfile %s does not exist!" % tarfile)
@@ -176,7 +176,7 @@ class Kcompile(CommandLineLoad):
                                   stdin=null, stdout=out, stderr=err)
             if ret:
                 raise rtevalRuntimeError(self, "kcompile setup failed: %d" % ret)
-        except KeyboardInterrupt, m:
+        except KeyboardInterrupt as m:
             self._log(Log.DEBUG, "keyboard interrupt, aborting")
             return
         self._log(Log.DEBUG, "ready to run")
@@ -197,7 +197,7 @@ class Kcompile(CommandLineLoad):
         else:
             self.__outfd = self.__errfd = self.__nullfd
 
-        if self._cfg.has_key('cpulist') and self._cfg.cpulist:
+        if 'cpulist' in self._cfg and self._cfg.cpulist:
             cpulist = self._cfg.cpulist
             self.num_cpus = len(expand_cpulist(cpulist))
         else:
@@ -206,7 +206,7 @@ class Kcompile(CommandLineLoad):
     def _WorkloadTask(self):
         for n in self.topology:
             if not self.buildjobs[n]:
-                raise RuntimeError, "Build job not set up for node %d" % int(n)
+                raise RuntimeError("Build job not set up for node %d" % int(n))
             if self.buildjobs[n].jobid is None or self.buildjobs[n].jobid.poll() is not None:
                 self._log(Log.INFO, "Starting load on node %d" % n)
                 self.buildjobs[n].run(self.__nullfd, self.__outfd, self.__errfd)

commit 1e79afef7d389db93db30078f5d15f8599a291cb

rteval: Add __contains__ in rtevalConfig
Add the __contains__ function to the rtevalCfgSection class  to make "in"
function correctly.

For example in cyclictest.py, self.__cfg is not a dictionary, it is an
instance of the rtevalCfgSection class, therefore
key in self.__cfg
does not work as expected. In order to make sure this is fixed
everywhere, instead of comparing to self.__cfg.keys() everywhere this is
used, implement the __contains__ function so that "in" works as
expected.

This bug was introduced by the commit fd3b732f714d ("rteval: 2to3
transformations")

Reported-by: Atsushi Nemoto <atsushi.nemoto@sord.co.jp>
Signed-off-by: John Kacur <jkacur@redhat.com>
Correction
Suggested-by: Jeff Epler <jepler@unpythonic.net>
Signed-off-by: John Kacur <jkacur@redhat.com>

rteval/rtevalConfig.py
@@ -126,6 +126,8 @@ class rtevalCfgSection:
             return self.__cfgdata[key]
         return None
 
+    def __contains__(self, key):
+        return key in self.__cfgdata
 
     def items(self):
         return list(self.__cfgdata.items())